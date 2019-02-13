ccdc8791771b3e65c4656efa55fe1052c4f66a4e
acerasino <acerasino@eidoo.io>
2019-02-13 17:57:54 +0100

    #7 Delete test for building transaction from Erc20TokenService

--- tests/unit/services/Erc20TokenService.test.js
+++ tests/unit/services/Erc20TokenService.test.js
@@ -1,24 +1,21 @@
 const Web3 = require('web3')
-const { EidooEthApiLib } = require('@eidoo/ethapi-lib')
 const sandbox = require('sinon').createSandbox()
+const { EidooEthApiLib } = require('@eidoo/ethapi-lib')
 
 const logger = require('../../../src/logger')
-const { InvalidEthereumAddress } = require('../../../src/utils/errors')
+
+const erc20Token = require('../../../abi/erc20Token')
+
 const Erc20TokenService = require('../../../src/services/Erc20TokenService')
-const MockTransactionDraftFactory = require('../../factories/MockTransactionDraftFactory')
+const Erc20TokenTransactionBuilder = require('../../../src/factories/Erc20TokenTransactionBuilder')
 const { TransactionLib } = require('../../../src/lib/TransactionLib')
 
 const providerUrl = 'FAKE_PROVIDER_URL'
 const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl))
 
-const erc20TokenSmartContractAddress = '0xd67cb05ef66d1c7c952656ddf5096e02281e3d2e'
 const tradingWalletSmartContractAddress = '0x0ba8f4ca1ba7a76a9da66e1629c24eb432aa96ba'
-const quantity = '500000000000000000000'
 const tokenAddress = '0xd67cb05ef66d1c7c952656ddf5096e02281e3d2d'
 
-afterEach(() => {
-  sandbox.restore()
-})
 const ethApiLibConf = {
   host: 'http:host.eidoo.io',
   port: 8080,
@@ -26,48 +23,22 @@ const ethApiLibConf = {
 }
 const ethApiLib = new EidooEthApiLib(ethApiLibConf)
 
-const transactionLibInstance = new TransactionLib(web3, logger, ethApiLib)
-let erc20TokenService
+const transactionLib = new TransactionLib(web3, logger, ethApiLib)
 
-beforeEach(() => {
-  erc20TokenService = new Erc20TokenService(
-    web3, { erc20TokenSmartContractAddress }, transactionLibInstance,
-  )
-})
+const erc20TokenSmartContractAddress = '0xd67cb05ef66d1c7c952656ddf5096e02281e3d2e'
 
-describe('getApproveTrasferTransactionDraft', () => {
-  test('returns correct transaction object', () => {
-    const personalWalletAddress = '0xd57cb05EF66d2C7C952656Ddf5096e02281E3d2e'
-    const encodedDataForApproveMethod = '0x095ea7b30000000000000000000000000ba8f4ca1ba7a76a9da66e1629c24eb432aa96ba00000000000000000000000000000000000000000000001b1ae4d6e2ef500000'
+const erc20TokenTransactionBuilder = new Erc20TokenTransactionBuilder(
+  web3,
+  { erc20TokenSmartContractAbi: erc20Token,
+    erc20TokenSmartContractAddress,
+    transactionLib,
+    logger },
+)
 
-    const expectedTransactionObjectDraft = MockTransactionDraftFactory
-      .build({ from: personalWalletAddress, data: encodedDataForApproveMethod, to: erc20TokenSmartContractAddress })
+const erc20TokenService = new Erc20TokenService(web3, transactionLib, erc20TokenTransactionBuilder, logger)
 
-    const result = erc20TokenService.getApproveTrasferTransactionDraft(personalWalletAddress, tradingWalletSmartContractAddress, quantity)
-
-    expect(result).toMatchObject(expectedTransactionObjectDraft)
-  })
-
-  const invalidPersonalWalletAddresses = [{}, [], 'I am not a valid ethereum address', 1, undefined, NaN, '0xNOOP']
-  test.each(invalidPersonalWalletAddresses)('raise InvalidEthereumAddress error if the personalWalletAddress is not a valid ethereum address %s',
-    (invalidPersonalWalletAddress) => {
-      try {
-        erc20TokenService.getApproveTrasferTransactionDraft(invalidPersonalWalletAddress, tradingWalletSmartContractAddress, quantity)
-      } catch (e) {
-        expect(e instanceof InvalidEthereumAddress).toBe(true)
-      }
-    })
-
-  const invalidTradingWalletAddresses = [{}, [], 'I am not a valid ethereum address', 1, undefined, NaN, '0xNOOP']
-  test.each(invalidTradingWalletAddresses)('raise InvalidEthereumAddress error if the tradingWalletAddress is not a valid ethereum address %s',
-    (invalidTradingWalletAddress) => {
-      try {
-        const validPersonalWalletAddress = '0xd57cb05ef66d2c7c952656ddf5096e02281e3d2e'
-        erc20TokenService.getApproveTrasferTransactionDraft(validPersonalWalletAddress, invalidTradingWalletAddress, quantity)
-      } catch (e) {
-        expect(e instanceof InvalidEthereumAddress).toBe(true)
-      }
-    })
+afterEach(() => {
+  sandbox.restore()
 })
 
 describe('approveTrasferAsync', () => {
@@ -87,7 +58,7 @@ describe('approveTrasferAsync', () => {
         low: '0',
       },
     }
-    sandbox.stub(transactionLibInstance, 'sign')
+    sandbox.stub(transactionLib, 'sign')
     sandbox.stub(ethApiLib, 'sendRawTransactionAsync').returns({ hash: expectedTransactionHash })
     sandbox.stub(ethApiLib, 'getAddressNonceAsync').returns(nonceResponse)
     sandbox.stub(ethApiLib, 'getEstimateGasAsync').returns(gasEstimationResponse)
