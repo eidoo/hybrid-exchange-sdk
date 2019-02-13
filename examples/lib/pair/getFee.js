5227a4a12d027f9b7d67b9039ea9921d4797cf2b
acerasino <acerasino@eidoo.io>
2019-02-13 11:24:17 +0100

    #1 Adjust getFee example

--- examples/lib/pair/getFee.js
+++ examples/lib/pair/getFee.js
@@ -1,8 +1,8 @@
 (async () => {
   const { PairService } = require('@eidoo/hybrid-exchange-sdk').services
   const pairService = new PairService()
-  const fromSymbol = 'EDO'
-  const toSymbol = 'ETH'
-  const fee = await pairService.getFeeAsync(fromSymbol, toSymbol)
+  const baseSymbol = 'EDO'
+  const quoteSymbol = 'ETH'
+  const fee = await pairService.getFeeAsync(baseSymbol, quoteSymbol)
   console.log(fee)
 })()
