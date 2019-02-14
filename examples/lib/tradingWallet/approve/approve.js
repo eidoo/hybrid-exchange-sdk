8b934861307edee1d41ec0513a7bcf1d960361bf
acerasino <acerasino@eidoo.io>
2019-02-14 11:13:03 +0100

    #7 Adjust example for approve

--- examples/lib/tradingWallet/approve/approve.js
+++ examples/lib/tradingWallet/approve/approve.js
@@ -8,7 +8,7 @@
   const erc20TokenService = new Erc20TokenService(web3, { erc20TokenSmartContractAddress: tokenAddress })
   const personalWalletAddress = '0xf6ccfafbe3ac8b6c82f39f54530a5ff7c0a1ed52'
   const tradingWalletAddress = '0xa3725d5a71db4550770d5c96b0104e40e8be8d23'
-  const toDeposit = '500000000000000000' // 0.5 ETH
+  const toDeposit = '500000000000000000'
   const privateKey = 'bf177acb6470f78c01209c1461fb0a104b2b0dbff9607d0b95b7e02f450b4142'
 
   const transactionHash = await erc20TokenService.approveTrasferAsync(
