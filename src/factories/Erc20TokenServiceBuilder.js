8b113e5167e8b883c39a9b877c0ee8179ec1667f
acerasino <acerasino@eidoo.io>
2019-02-14 17:57:53 +0100

    #9 Add Erc20TokenServiceBuilder in factories

---
+++ src/factories/Erc20TokenServiceBuilder.js
@@ -0,0 +1,39 @@
+const Web3 = require('web3')
+
+const logger = require('../logger')
+const Erc20TokenTransactionBuilder = require('./Erc20TokenTransactionBuilder')
+
+const { TransactionLib } = require('../lib/TransactionLib')
+const { Erc20TokenService } = require('../services')
+
+const providerUrl = 'urlToProvider'
+const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl))
+
+const transactionLibInstance = new TransactionLib(web3, logger)
+
+/**
+   * Class representing a simple factory to build Erc20TokenService object.
+   */
+
+class Erc20TokenServiceBuilder {
+  constructor(tokenAddress) {
+    if (!tokenAddress) {
+      const errorMessage = `Invalid "tokenAddress" value: ${tokenAddress}`
+      throw new TypeError(errorMessage)
+    }
+
+    this.tokenAddress = tokenAddress
+  }
+
+  build() {
+    const erc20TokenTransactionBuilder = new Erc20TokenTransactionBuilder(
+      web3,
+      { erc20TokenSmartContractAddress: this.tokenAddress,
+        transactionLib: transactionLibInstance },
+      logger,
+    )
+    return new Erc20TokenService(web3, transactionLibInstance, erc20TokenTransactionBuilder, logger)
+  }
+}
+
+module.exports = Erc20TokenServiceBuilder
--- src/factories/index.js
+++ src/factories/index.js
@@ -1,9 +1,11 @@
 const Erc20TokenTransactionBuilder = require('./Erc20TokenTransactionBuilder')
+const Erc20TokenServiceBuilder = require('./Erc20TokenServiceBuilder')
 const OrderPayloadBuilder = require('./OrderPayloadBuilder')
 const TradingWalletTransactionBuilder = require('./TradingWalletTransactionBuilder')
 
 module.exports = {
   Erc20TokenTransactionBuilder,
+  Erc20TokenServiceBuilder,
   OrderPayloadBuilder,
   TradingWalletTransactionBuilder,
 }
