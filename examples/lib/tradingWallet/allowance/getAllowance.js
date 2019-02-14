8b31135268ffcca4bccde03210513dd41b2bb98c
acerasino <acerasino@eidoo.io>
2019-02-14 17:58:19 +0100

    #9 Adjust approve and allowance example

--- examples/lib/tradingWallet/allowance/getAllowance.js
+++ examples/lib/tradingWallet/allowance/getAllowance.js
@@ -1,26 +1,12 @@
 (async () => {
-  const { Erc20TokenService } = require('@eidoo/hybrid-exchange-sdk').services
-  const { TransactionLib } = require('@eidoo/hybrid-exchange-sdk').lib.TransactionLib
-  const { Erc20TokenTransactionBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
-
-  const Web3 = require('web3')
-
-  const providerUrl = 'providerUrl'
-  const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl))
+  const { Erc20TokenServiceBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
 
   const tokenAddress = '0x9727e2fb13f7f42d5a6f1a4a9877d4a7e0404d6a'
   const personalWalletAddress = '0xf6ccfafbe3ac8b6c82f39f54530a5ff7c0a1ed52'
   const tradingWalletAddress = '0xa3725d5a71db4550770d5c96b0104e40e8be8d23'
 
-  const transactionLib = new TransactionLib()
-
-  const erc20TokenTransactionBuilder = new Erc20TokenTransactionBuilder(
-    web3,
-    { erc20TokenSmartContractAddress: tokenAddress,
-      transactionLib },
-  )
-
-  const erc20TokenService = new Erc20TokenService(web3, transactionLib, erc20TokenTransactionBuilder)
+  const erc20TokenServiceBuilder = new Erc20TokenServiceBuilder(tokenAddress)
+  const erc20TokenService = erc20TokenServiceBuilder.build()
   const allowance = await erc20TokenService.getAllowanceAsync(
     personalWalletAddress,
     tradingWalletAddress,
