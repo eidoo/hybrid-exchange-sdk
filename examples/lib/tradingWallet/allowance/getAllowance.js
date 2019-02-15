331821c542fe31503f8c0185a04392105fbe0b72
acerasino <acerasino@eidoo.io>
2019-02-15 10:39:43 +0100

    #9 Adjust examples using TradingWalletServiceBuilder

--- examples/lib/tradingWallet/allowance/getAllowance.js
+++ examples/lib/tradingWallet/allowance/getAllowance.js
@@ -2,8 +2,8 @@
   const { Erc20TokenServiceBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
 
   const tokenAddress = '0x9727e2fb13f7f42d5a6f1a4a9877d4a7e0404d6a'
-  const personalWalletAddress = '0xf6ccfafbe3ac8b6c82f39f54530a5ff7c0a1ed52'
-  const tradingWalletAddress = '0xa3725d5a71db4550770d5c96b0104e40e8be8d23'
+  const personalWalletAddress = '0xcf4b07a79b5d29988f488f30c4a676ecaad35c02'
+  const tradingWalletAddress = '0x9c6d1840381cc570235a4ed867bf8465e32ce753'
 
   const erc20TokenServiceBuilder = new Erc20TokenServiceBuilder(tokenAddress)
   const erc20TokenService = erc20TokenServiceBuilder.build()
--- examples/lib/tradingWallet/depositEth/depositEther.js
+++ examples/lib/tradingWallet/depositEth/depositEther.js
@@ -1,15 +1,12 @@
 (async () => {
-  const { TradingWalletService } = require('@eidoo/hybrid-exchange-sdk').services
-  const Web3 = require('web3')
+  const { TradingWalletServiceBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
 
-  const providerUrl = 'providerUrl'
-  const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl))
-  const tradingWalletService = new TradingWalletService(web3)
+  const tradingWalletService = TradingWalletServiceBuilder.build()
 
-  const personalWalletAddress = '0xf6ccfafbe3ac8b6c82f39f54530a5ff7c0a1ed52'
-  const tradingWalletAddress = '0xa3725d5a71db4550770d5c96b0104e40e8be8d23'
-  const privateKey = 'bf177acb6470f78c01209c1461fb0a104b2b0dbff9607d0b95b7e02f450b4142'
-  const quantityToDeposit = '500000000000000000'
+  const personalWalletAddress = '0xcf4b07a79b5d29988f488f30c4a676ecaad35c02'
+  const tradingWalletAddress = '0x9c6d1840381cc570235a4ed867bf8465e32ce753'
+  const privateKey = '0x4c7ee440ad699493b22732031e4a3277d2d8aa834b727aa0b358e3310aa37384'
+  const quantityToDeposit = '100000000000000000'
 
   const transactionHash = await tradingWalletService.depositEtherAsync(
     personalWalletAddress,
