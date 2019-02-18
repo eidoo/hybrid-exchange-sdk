6a5ab256d41b8478ca261024642c1dc82b1bd042
acerasino <acerasino@eidoo.io>
2019-02-18 16:17:56 +0100

    #11 Add Example

--- examples/lib/tradingWallet/depositToken/depositTokenWithApprove.js
+++ examples/lib/tradingWallet/depositToken/depositTokenWithApprove.js
@@ -1,8 +1,9 @@
 (async () => {
   // const { TradingWalletServiceBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
-  const { TradingWalletServiceBuilder } = require('../../../../index').factories
+  // const { TradingWalletFacade } = require('@eidoo/hybrid-exchange-sdk').facades
 
-  const tradingWalletService = TradingWalletServiceBuilder.build()
+  const { Erc20TokenServiceBuilder, TradingWalletServiceBuilder } = require('../../../../index').factories
+  const { TradingWalletFacade } = require('../../../../index').facades
 
   const tokenAddress = '0x9727e2fb13f7f42d5a6f1a4a9877d4a7e0404d6a'
   const personalWalletAddress = '0xcf4b07a79b5d29988f488f30c4a676ecaad35c02'
@@ -10,12 +11,14 @@
   const privateKey = '0x4c7ee440ad699493b22732031e4a3277d2d8aa834b727aa0b358e3310aa37384'
   const quantityToDeposit = '500000000000000000'
 
-  const transactionHash = await tradingWalletService.depositTokenAsync(
+  const erc20TokenServiceBuilder = new Erc20TokenServiceBuilder(tokenAddress)
+  const tradingWalletFacade = new TradingWalletFacade(TradingWalletServiceBuilder.build(), erc20TokenServiceBuilder.build())
+
+  await tradingWalletFacade.depositTokenAsync(
     personalWalletAddress,
     tradingWalletAddress,
     quantityToDeposit,
     tokenAddress,
     privateKey,
   )
-  console.log(transactionHash)
 })()
