85ba160fdbb50c916de12ecf2483daeda7d32c52
acerasino <acerasino@eidoo.io>
2019-02-22 12:35:53 +0100

    #11 Adjust deposit with approve example

--- examples/lib/tradingWallet/depositToken/depositTokenWithApprove.js
+++ examples/lib/tradingWallet/depositToken/depositTokenWithApprove.js
@@ -1,6 +1,11 @@
 (async () => {
-  const { TradingWalletServiceBuilder, Erc20TokenServiceBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
+  const { TradingWalletTransactionBuilder, Erc20TokenServiceBuilder, Erc20TokenTransactionBuilder } = require('@eidoo/hybrid-exchange-sdk').factories
   const { TradingWalletFacade } = require('@eidoo/hybrid-exchange-sdk').facades
+  const { TransactionLib } = require('@eidoo/hybrid-exchange-sdk').lib.TransactionLib
+
+  const Web3 = require('web3')
+  const providerUrl = 'providerUrl'
+  const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl))
 
   const tokenAddress = '0x9727e2fb13f7f42d5a6f1a4a9877d4a7e0404d6a'
   const personalWalletAddress = '0xcf4b07a79b5d29988f488f30c4a676ecaad35c02'
@@ -8,8 +13,19 @@
   const privateKey = '0x4c7ee440ad699493b22732031e4a3277d2d8aa834b727aa0b358e3310aa37384'
   const quantityToDeposit = '500000000000000000'
 
+  const tradingWalletTransactionBuilder = new TradingWalletTransactionBuilder(web3)
   const erc20TokenServiceBuilder = new Erc20TokenServiceBuilder(tokenAddress)
-  const tradingWalletFacade = new TradingWalletFacade(TradingWalletServiceBuilder.build(), erc20TokenServiceBuilder.build())
+  const transactionLib = new TransactionLib()
+  const erc20TokenTransactionBuilder = new Erc20TokenTransactionBuilder(
+    web3,
+    { erc20TokenSmartContractAddress: tokenAddress,
+      transactionLib },
+  )
+  const tradingWalletFacade = new TradingWalletFacade(
+    tradingWalletTransactionBuilder,
+    erc20TokenServiceBuilder.build(),
+    erc20TokenTransactionBuilder,
+  )
 
   await tradingWalletFacade.depositTokenAsync(
     personalWalletAddress,
