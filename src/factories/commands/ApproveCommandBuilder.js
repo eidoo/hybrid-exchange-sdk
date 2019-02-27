5cecfa2297ff4b4ba3cd72c5f775423de1f9bf67
andreafspeziale <andreaf.speziale@gmail.com>
2019-02-27 15:50:14 +0100

    #40 Add command builders

---
+++ src/factories/commands/ApproveCommandBuilder.js
@@ -0,0 +1,23 @@
+const ApproveCommand = require('../../commands/ApproveCommand')
+const ApproveCommandValidator = require('../../validators/ApproveCommandValidator')
+const { PrivateKeyService } = require('../../services/PrivateKeyService')
+const PrivateKeyValidator = require('../../validators/PrivateKeyValidator')
+
+const logger = require('../../logger')
+
+const privateKeyService = new PrivateKeyService(logger)
+const privateKeyValidator = new PrivateKeyValidator(logger)
+
+/**
+ * Class representing a simple factory to build ApproveCommandBuilder object.
+ */
+class ApproveCommandBuilder {
+  static build() {
+    const approveCommandValidator = new ApproveCommandValidator(logger)
+    const approveCommand = new ApproveCommand(logger, approveCommandValidator,
+      privateKeyService, privateKeyValidator)
+    return approveCommand
+  }
+}
+
+module.exports = ApproveCommandBuilder
---
+++ src/factories/commands/GetBalanceCommandBuilder.js
@@ -0,0 +1,25 @@
+const logger = require('../../logger')
+const { PrivateKeyService } = require('../../services/PrivateKeyService')
+
+const GetBalanceCommand = require('../../commands/GetBalanceCommand')
+const GetBalanceCommandValidator = require('../../validators/GetBalanceCommandValidator')
+const PrivateKeyValidator = require('../../validators/PrivateKeyValidator')
+const TradingWalletServiceBuilder = require('../TradingWalletServiceBuilder')
+
+const privateKeyService = new PrivateKeyService(logger)
+const privateKeyValidator = new PrivateKeyValidator(logger)
+const tradingWalletService = TradingWalletServiceBuilder.build()
+
+/**
+ * Class representing a simple factory to build GetBalanceCommandBuilder object.
+ */
+class GetBalanceCommandBuilder {
+  static build() {
+    const getBalanceCommandValidator = new GetBalanceCommandValidator(logger)
+    const getBalanceCommand = new GetBalanceCommand(logger, tradingWalletService,
+      getBalanceCommandValidator, privateKeyService, privateKeyValidator)
+    return getBalanceCommand
+  }
+}
+
+module.exports = GetBalanceCommandBuilder
