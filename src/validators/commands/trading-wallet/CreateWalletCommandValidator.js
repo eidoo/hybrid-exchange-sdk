116cfc4795d33c90ec793a566e5894ae192500d5
andreafspeziale <andreaf.speziale@gmail.com>
2019-03-08 16:23:47 +0100

    #65 Update create wallet command validator

--- src/validators/commands/trading-wallet/CreateWalletCommandValidator.js
+++ src/validators/commands/trading-wallet/CreateWalletCommandValidator.js
@@ -3,7 +3,8 @@ const BaseValidator = require('../../BaseValidator')
 
 const createWalletSchema = customJoiValidator.object()
   .keys({ personalWalletAddress: customJoiValidator.address().ethereum().required(),
-    privateKeyPath: customJoiValidator.path().existFile(),
+    keystoreFilePath: customJoiValidator.path().existFile().required(),
+    keystorePassword: customJoiValidator.string().required(),
     draft: customJoiValidator.boolean(),
     rawTx: customJoiValidator.boolean() })
 
